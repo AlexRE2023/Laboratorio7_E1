f110f74a58b51a47bb947c48e28c26d3c8d94bcc
alexi <alexi@ALEXANDER>
2023-10-04 16:49:12 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio7_E1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio7_E1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el valor de n: ");
+            int n = int.Parse(Console.ReadLine());
+            double i = 1;
+
+            while (i <= n)
+            {
+                double resultado = (16 - i) / (3 * i); i++;
+                Console.WriteLine("Resultado: " + resultado);
+              
+            }
+            Console.ReadKey();
+        }
+    }
+}
